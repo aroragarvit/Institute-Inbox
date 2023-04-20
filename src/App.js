ca72505dedbe116f5204af06906a60ab0f3d02bd
garvit <agarvit1142000@gmail.com>
2023-04-20 23:59:36 +0530

    Created login template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import Login from "./pages/Login";
 function App() {
-  return <div className="App">Hello Guys halat bhot kharab ha guys</div>;
+  return (
+    <div className="App">
+      Hello Guys halat bhot kharab ha guys
+      <Login />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,100 @@
+import { LockOutlined, UserOutlined, MailOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
+
+const Login = () => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        style={{
+          maxWidth: "400px",
+          width: "100%",
+          padding: "20px",
+          border: "1px solid #e8e8e8",
+        }} // Add border property here
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="email" // Add email input field
+          rules={[
+            {
+              required: true,
+              message: "Please input your Email!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<MailOutlined className="site-form-item-icon" />} // Use MailOutlined icon for email input field
+            placeholder="Email"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
