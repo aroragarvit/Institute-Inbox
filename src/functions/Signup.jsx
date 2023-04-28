90e82a8e8394d08090c5ed4032a5a18bbe038bfe
Rakshit Arora <rakshit087@gmail.com>
2023-04-29 02:54:15 +0530

    removed useEffect and made signup async

--- src/functions/Signup.jsx
+++ src/functions/Signup.jsx
@@ -1,39 +1,29 @@
-import { useEffect } from "react";
 import { auth } from "../config/firebase";
-import { db } from "../config/firebase";
 
-export const signup = (values) => {
-  return auth
-    .createUserWithEmailAndPassword(values.email, values.password)
-    .then((userCredential) => {
-      var user = userCredential.user;
-      console.log(user);
-
-      return user.sendEmailVerification().then(function () {
-        console.log("Email sent");
-
-        return new Promise((resolve, reject) => {
-          useEffect(() => {
-            const unsubscribe = auth.onAuthStateChanged((user) => {
-              if (user && user.emailVerified) {
-                unsubscribe();
-                resolve(user);
-              }
-            });
-
-            setTimeout(() => {
-              // NOt working properly
-              unsubscribe();
-              auth.signOut();
-              reject(new Error("Please verify your email to sign up."));
-            }, 5 * 60 * 1000);
-          }, []);
-
-          return user;
-        });
+export const signup = async (values) => {
+  try {
+    const userCredential = await auth.createUserWithEmailAndPassword(
+      values.email,
+      values.password
+    );
+    var user = userCredential.user;
+    await user.sendEmailVerification();
+    alert("Email sent");
+    return await new Promise((resolve, reject) => {
+      auth.onAuthStateChanged((user_1) => {
+        if (user_1 && user_1.emailVerified) {
+          alert("Email Verified");
+          resolve(user_1);
+        }
       });
-    })
-    .catch((error) => {
-      console.log(error.code, error.message);
+      setTimeout(async () => {
+        await user.delete();
+        await auth.signOut();
+        reject(new Error("Please verify your email to sign up."));
+      }, 5 * 60 * 1000);
+      return user;
     });
+  } catch (error) {
+    console.log(error.code, error.message);
+  }
 };
