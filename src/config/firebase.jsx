6ee6c7ed225b5f48af93ec4e37b839fe598dc744
garvit <agarvit1142000@gmail.com>
2023-04-29 23:13:33 +0530

    storage setup in firebase config

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -2,7 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/messaging";
 import "firebase/compat/firestore";
-
+import "firebase/compat/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyAiKT--M5urs0FkSbVhRShgPBAonmo8SY4",
   authDomain: "institute-inbox.firebaseapp.com",
@@ -11,6 +11,7 @@ const firebaseConfig = {
   messagingSenderId: "398645319055",
   appId: "1:398645319055:web:ad2bef3587e36f1f6227fa",
   measurementId: "G-MN4S08S1ZY",
+  storageBucket: "gs://institute-inbox.appspot.com",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -18,5 +19,6 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const messaging = firebase.messaging();
 const firestore = firebase.firestore();
+const storage = firebase.storage();
 
-export { auth, messaging, firestore };
+export { auth, messaging, firestore, storage };
