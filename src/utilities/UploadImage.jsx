393b941172017e6ffc2aa48e9a6b1604a72c7905
garvit <agarvit1142000@gmail.com>
2023-04-29 23:13:51 +0530

    created a function for uploading image

---
+++ src/utilities/UploadImage.jsx
@@ -0,0 +1,18 @@
+import { storage } from "../config/firebase";
+
+export const uploadImage = async (file, uid) => {
+  try {
+    const storageRef = storage.ref();
+    const imageFolderRef = storageRef.child("images");
+
+    const uidRef = imageFolderRef.child(uid);
+    const imageRef = uidRef.child(file.name);
+
+    await imageRef.put(file);
+    const url = await imageRef.getDownloadURL();
+    return url;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
