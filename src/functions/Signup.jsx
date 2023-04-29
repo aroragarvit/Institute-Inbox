acfc8d4ebe223c492590968df421c34f9b35dae2
garvit <agarvit1142000@gmail.com>
2023-04-30 01:26:55 +0530

    conflict  resolved

--- src/functions/Signup.jsx
+++ src/functions/Signup.jsx
@@ -1,39 +1,58 @@
-import { useEffect } from "react";
-import { auth } from "../config/firebase";
-import { db } from "../config/firebase";
+import { auth, firestore, storage } from "../config/firebase";
+import { uploadImage } from "../utilities/UploadImage";
+export const signup = async (values) => {
+  try {
+    const userCredential = await auth.createUserWithEmailAndPassword(
+      values.email,
+      values.password
+    );
+    const user = userCredential.user;
+    console.log(user);
 
-export const signup = (values) => {
-  return auth
-    .createUserWithEmailAndPassword(values.email, values.password)
-    .then((userCredential) => {
-      var user = userCredential.user;
-      console.log(user);
+    // Send email verification request
+    await user.sendEmailVerification();
+    console.log("Email sent");
 
-      return user.sendEmailVerification().then(function () {
-        console.log("Email sent");
+    // wait for 30 seconds for the user to verify their email
+    let i = 0;
+    while (!user.emailVerified && i < 30) {
+      // 300 seconds = 5 minutes
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second
+      i++;
+      await user.reload(); // Reload the user object to update the emailVerified property
+    }
 
-        return new Promise((resolve, reject) => {
-          useEffect(() => {
-            const unsubscribe = auth.onAuthStateChanged((user) => {
-              if (user && user.emailVerified) {
-                unsubscribe();
-                resolve(user);
-              }
-            });
+    if (!user.emailVerified) {
+      // If the user's email is still not verified after 5 minutes, delete the user and throw an error
+      await user.delete();
+      throw new Error("Please verify your email to sign up.");
+    }
 
-            setTimeout(() => {
-              // NOt working properly
-              unsubscribe();
-              auth.signOut();
-              reject(new Error("Please verify your email to sign up."));
-            }, 5 * 60 * 1000);
-          }, []);
+    const url = await uploadImage(values.image, user.uid);
 
-          return user;
-        });
-      });
-    })
-    .catch((error) => {
-      console.log(error.code, error.message);
+    // Update the user's profile
+    await user.updateProfile({
+      displayName: values.name,
+      email: values.email,
+      photoURL: url,
     });
+
+    // Create a new document in the users collection with the uid of the user
+    const usersCollection = firestore.collection("users");
+    await usersCollection.doc(user.uid).set({
+      email: user.email,
+      name: values.name,
+      photoURL: url,
+      phoneNumber: values.phone,
+      uid: user.uid,
+      hostel: values.Hostel,
+      block: values.Block,
+      gender: values.gender,
+    });
+
+    return user;
+  } catch (error) {
+    console.log(error.code, error.message);
+    throw error; // re-throwing the error to handle it in the calling function
+  }
 };
