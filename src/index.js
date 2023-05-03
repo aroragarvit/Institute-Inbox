4759de8c2c32d1284be8e74ce9a00651afcf1982
Rakshit Arora <rakshit087@gmail.com>
2023-05-03 19:11:45 +0530

    removed extra import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AuthContext } from "./context/AuthContext.jsx";
-import { useContext } from "react";
-
 import App from "./App.js";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
