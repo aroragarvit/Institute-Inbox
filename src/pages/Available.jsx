defe625c5b3624eace2eb1e2028e49ef17a7d7fe
Rakshit Arora <rakshit087@gmail.com>
2023-05-03 19:05:53 +0530

    added Navbar

--- src/pages/Available.jsx
+++ src/pages/Available.jsx
@@ -1,5 +1,17 @@
+import Navbar from "../components/Navbar";
+
 const Available = () => {
   {
-    return <div></div>;
+    return (
+      <div style={{
+        padding: "0 8rem",
+        height: "100vh",
+        backgroundColor: "#a7bcff",
+      }}>
+        <Navbar />
+      </div>
+    );
   }
 };
+
+export default Available;
