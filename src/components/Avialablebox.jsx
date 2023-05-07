afb024ca38b3f709f7ccc7ba44d65fba5006bff2
garvit <agarvit1142000@gmail.com>
2023-05-07 06:43:10 +0530

    Web designing  of available box

--- src/components/Avialablebox.jsx
+++ src/components/Avialablebox.jsx
@@ -1,22 +1,28 @@
+import React from "react";
+
 const Availablebox = (props) => {
   return (
     <div
       style={{
         display: "flex",
-        width: "729px",
+        maxWidth: "729px",
         height: "161px",
-        border: " 1px solid #E7E7E7",
+        border: "1px solid #E7E7E7",
         borderRadius: "4px",
         backgroundColor: "#FEFEFE",
+        flexDirection: "column",
+        padding: "0px",
+        margin: "0px",
       }}
     >
       <div
         style={{
           display: "flex",
           flexDirection: "row",
-          justifyContent: "center",
+          justifyContent: "flex-start",
           height: "35px",
           alignItems: "center",
+          width: "auto",
           marginLeft: "12px",
           marginTop: "18px",
         }}
@@ -37,8 +43,8 @@ const Availablebox = (props) => {
             display: "flex",
             width: "auto",
             height: "14px",
-            fontfamily: "montserrat",
-            fontsize: "16px",
+            fontFamily: "montserrat",
+            fontSize: "16px",
             fontWeight: "600",
             lineHeight: "14px",
             fontStyle: "normal",
@@ -50,8 +56,74 @@ const Availablebox = (props) => {
           Hello my world
         </div>
       </div>
-      <div style={{ display: "flex" }}>hhhhh</div>
+      <div
+        style={{
+          display: "flex",
+          width: "auto",
+          height: "14px",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "12px",
+          marginBottom: "12px",
+          marginLeft: "12px",
+          marginRight: "12px",
+          fontFamily: "montserrat",
+          fontSize: "14px",
+          fontWeight: "500",
+          lineHeight: "14px",
+          fontStyle: "normal",
+          color: "##2E2C34",
+        }}
+      >
+        ZAKIR -C
+      </div>
+      <div
+        style={{
+          display: "flex",
+          width: "auto",
+          height: "60px", // Set a fixed height of 60px for the container
+          marginBottom: "12px",
+          marginLeft: "12px",
+          marginRight: "12px",
+          fontFamily: "montserrat",
+          fontSize: "14px",
+          fontWeight: "500",
+          lineHeight: "20px",
+          fontStyle: "normal",
+          color: "##2E2C34",
+          overflow: "hidden", // Hide the text that overflows
+        }}
+      >
+        <div style={{ overflowY: "scroll", height: "100%" }}>
+          {/* Enclose the text within another div */}
+          Hello my name is Garvit Arora and I am a student of VIT Bhopal
+          University currently I am in 2nd year of my college and I am pursuing
+          Btech in Computer Science and Engineering. I want to become a full
+          stack developer and I am currently learning ReactJS and NodeJS.
+        </div>
+      </div>
+      <style>
+        {`
+          ::-webkit-scrollbar {
+            width: 8px;
+          }
+          
+          ::-webkit-scrollbar-track {
+            background: #F5F5F5;
+          }
+          
+          ::-webkit-scrollbar-thumb {
+            background: #ccc;
+            border-radius: 4px;
+          }
+          
+          ::-webkit-scrollbar-thumb:hover {
+            background: #aaa;
+          }
+        `}
+      </style>
     </div>
   );
 };
+
 export default Availablebox;
