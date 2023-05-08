7482cb53e0d678e95ed9e411f2ca82404be3b27f
Rakshit Arora <rakshit087@gmail.com>
2023-05-08 11:56:14 +0530

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,55 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { LogoutOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
+
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
   return (
-    <div className="navbar">
-      <span className="logo"> CollegeChat</span>
-      <div className="user">
-        <img src="https://picsum.photos/200" alt="user" />
-        <span>Username</span>
-        <button>Logout</button>
+    <div style={{
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: "1rem 2rem",
+      borderBottom: "1px solid #E5E5E5",
+      borderRadius: "0 0 0.5rem 0.5rem",
+      position: "sticky",
+      top: "0",
+      zIndex: "100",
+      backgroundColor: "#fff"
+      
+    }}>
+      <span className="logo">Welcome! {user.email} </span>
+      <div style={{
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: "#F5F5F5",
+        padding: "0.5rem 1rem",
+        borderRadius: "1rem",
+      }}>
+        <Avatar
+          style={{
+            backgroundColor: "#7F56D8",
+            marginRight: "1rem",
+          }}
+          size={40}
+          src={user.photoURL}
+        >
+          {user.email[0].toUpperCase()}
+        </Avatar>
+        <span
+          style={{
+            fontWeight: "bold",
+            fontSize: "14px",
+          }}
+
+        >{user.email}</span>
+        <LogoutOutlined style={{
+          marginLeft: "1rem",
+          fontSize: "1.5rem",
+          cursor: "pointer",
+      
+        }} />
       </div>
     </div>
   );
