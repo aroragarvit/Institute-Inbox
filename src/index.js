c92e3016b9b2a2416e7bf0f2f925c4e8f004cd5c
Rakshit Arora <rakshit087@gmail.com>
2023-05-08 11:34:18 +0530

    configured antd theme

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.js";
+import { ConfigProvider } from "antd";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#7F56D8",
+            colorBgTextHover: "#F3E7F9",
+            colorBgTextActive: "#eac5fc",
+            controlItemBgHover: "#F3E7F9",
+            controlItemBgActive: "#eac5fc",
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </AuthProvider>
   </React.StrictMode>
 );
