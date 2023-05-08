f3daddf89ca5aa0aaec6cc4df9c10dd5ae7b1c9d
Rakshit Arora <rakshit087@gmail.com>
2023-05-08 11:24:28 +0530

    added tabs to available card

--- src/pages/Available.jsx
+++ src/pages/Available.jsx
@@ -1,3 +1,6 @@
+import { AvailableCard } from "../components/AvailableCard";
+import { Tabs } from "antd";
+import { MessageOutlined, CheckOutlined } from "@ant-design/icons";
 import Availablebox from "../components/Avialablebox";
 import AvailableButton from "../utilities/AvailableButton_";
 import { fetchUsers } from "../utilities/FetchAvailableUsers .jsx";
@@ -28,6 +31,82 @@ const Available = () => {
       >
         <Availablebox />
         <AvailableButton />
+        <Tabs
+          style={{
+            backgroundColor: "white",
+            padding: "0.5rem 1rem",
+            borderRadius: "0.5rem",
+            marginTop: "1rem",
+          }}
+          defaultActiveKey="1"
+          items={[
+            {
+              key: "1",
+              label: (
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <MessageOutlined
+                    style={{
+                      color: "#7F56D8",
+                      marginRight: "0.5rem",
+                    }}
+                  />
+                  <p>Available</p>
+                </div>
+              ),
+              children:
+                users && users.length > 0 ? (
+                  users.map((user, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        margin: "1rem 0",
+                      }}
+                    >
+                      <AvailableCard
+                        name={user.name}
+                        avatar={user.avatar}
+                        nameOfHostal={user.nameOfHostal}
+                        description={user.description}
+                        timeOfPost={user.timeOfPost}
+                        uid={user.uid}
+                      />
+                    </div>
+                  ))
+                ) : (
+                  <div style={{
+                    padding: "1rem 0",
+                  }}>No users available</div>
+                ),
+            },
+            {
+              key: "2",
+              label: (
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <CheckOutlined
+                    style={{
+                      color: "#7F56D8",
+                      marginRight: "0.5rem",
+                    }}
+                  />
+                  <p>Resolved</p>
+                </div>
+              ),
+              children: <div style={{
+                padding: "1rem 0",
+              }}>Feature Coming Soon...</div>,
+            },
+          ]}
+        />
       </div>
     );
   }
