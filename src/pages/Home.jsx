9b8f46058fdef343475f1ab95763e620c4fbaab1
Rakshit Arora <rakshit087@gmail.com>
2023-05-12 20:06:47 +0530

    removed sidebar and added navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react";
-import Sidebar from "../components/Sidebar.jsx";
 import Chat from "../components/Chat.jsx";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => (document.body.style.overflow = "scroll");
-  });
+  // useEffect(() => {
+  //   document.body.style.overflow = "hidden";
+  //   return () => (document.body.style.overflow = "scroll");
+  // });
   return (
-    <div className="home">
-      <div className=" container">
-        <Sidebar></Sidebar>
-        <Chat></Chat>
+    <div
+      style={{
+        padding: "0 8rem",
+        height: "100vh",
+        backgroundColor: "#a7bcff",
+      }}
+    >
+      <Navbar />
+      <div className="home">
+        <div className="container">
+          <Chat></Chat>
+        </div>
       </div>
     </div>
   );
