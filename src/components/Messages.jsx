32f8cbb03b7a7fc3347924fe188098aba7e6dfb2
garvit <agarvit1142000@gmail.com>
2023-05-15 02:06:25 +0530

    Retriving messages of two  users from  database an passing as prop to Message component

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,22 +1,36 @@
 import Message from "./Message.jsx";
+import { useState, useEffect } from "react";
+import { onSnapshot, collection, doc } from "firebase/firestore";
+import { firestore } from "../config/firebase.jsx";
 
-const Messages = () => {
+const Messages = (props) => {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const unsub = onSnapshot(
+      doc(firestore, "chats", props.combinedID),
+      (document) => {
+        document.exists() && setMessages(document.data().messages);
+      }
+    );
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  console.log("message");
+
+  console.log(messages);
+
   return (
     <div className="messages">
-      <Message message="True" />
-      <Message message="Not true" isOwner />
-      <Message message="True" />
-      <Message message="Not true" isOwner />
-      <Message message="True" />
-      <Message message="Not true" isOwner />
-      <Message message="True" />
-      <Message message="Not true" isOwner />
-      <Message message="True" />
-      <Message message="Not true" isOwner />
-      <Message message="True" />
-      <Message message="Not true" isOwner />
-      <Message message="True" />
-      <Message message="Not true" isOwner />
+      {messages.map(({ id, senderId, text, date }) => (
+        <Message
+          message={text}
+          key={id}
+          date={date}
+          senderId={senderId}
+        ></Message>
+      ))}
     </div>
   );
 };
