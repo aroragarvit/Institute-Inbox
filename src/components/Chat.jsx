9f896298a07c63e9397059d7932a3bf70b034456
garvit <agarvit1142000@gmail.com>
2023-05-15 02:05:24 +0530

    Messages and Input components as children of  Chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,15 @@
 import Messages from "./Messages.jsx";
 import Input from "./Input.jsx";
-const chat = function () {
+const chat = function (props) {
+  console.log("combinedID");
+  console.log(props.combinedID);
   return (
     <div className="chat">
       <div className="chatInfo">
         <span>Jane</span>
       </div>
-      <Messages />
-      <Input />
+      <Messages combinedID={props.combinedID} />
+      <Input combinedID={props.combinedID} />
     </div>
   );
 };
